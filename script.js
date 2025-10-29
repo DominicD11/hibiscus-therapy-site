1838b5bf587e1a817ca1c88d062a743be0360278
DominicD11 <dominicdiwayan@gmail.com>
2025-10-30 00:59:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function bookSession() {
+  alert("Thank you for your interest! We'll contact you soon to schedule your session.");
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  alert("Your message has been sent. We'll get back to you shortly!");
+}
